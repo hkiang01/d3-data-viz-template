6a4c01f4d85a7edb904c4ffd26804af5d3895a5a
Harrison Kiang <hkiang01@gmail.com>
2017-07-26 21:53:56 -0400

    conclusion, narrative viz description, template credit

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,8 @@ class App extends Component {
                         <p>Another issue plaguing the city is its declining labor force. According to a recent <a href="http://www.chicagotribune.com/business/ct-chicago-metro-unemployment-may-0623-biz-20170622-story.html">report</a>, also by the Chicago Tribune, “Chicago metro area’s labor force - which includes everyone working or looking for work - shrank by about 70,000.”</p>
                         <p>One’s intuition may lead them to believe that these two issues are related. The purpose of this visualization is to evaluate whether there is in fact a relationship between unemployment and homicides in various Chicago neighborhoods.</p>
 
+                        <br/>
+
                         <h3>Data</h3>
                         <p>The City of Chicago has made public, through data.com, select public health indicators by Chicago community area. The data sources below were used:</p>
                         <ul>
@@ -52,14 +54,26 @@ class App extends Component {
                         </ul>
                         <p>Chicago Community Area boundaries comes in kml format and was transferred into geojson, a popular format for encoding a variety of geographic data structures. The public health indicators examined were Assault (Homicide) and Unemployment. Homicides per 100l residents between 2005-2009 were examined, as well as unemployment for residents 16 years or older between 2007-2011.</p>
 
+                        <br/>
+
+                        <h3>Hybrid Structure: Martini Glass and Drill Down Story</h3>
+                        <p>The premise, data, and other description sections supply the author-controlled content, in which the context of the problem and information on each of the visualizations. The relationship between each of the visualizations and the interactions that implement them are also described. See “Visualizations and Scenes”.</p>
+                        <p>Drilling down to obtain data about each community is possible by exercising the interactions that exist between each of the scenes in the visualization. See “Annotations and Triggers: Tooltips”.</p>
+
+                        <br/>
+
                         <h3>Visualizations and Scenes</h3>
                         <p>Three interactive visualizations are provided below, all built using d3.js. The color schemes for each of the three visualizations are the same, allowing the user to associate color with the data points that they are interested, making it possible for the user to visually compare data points, providing another dimension of information rather than just a data record in a csv file.</p>
                         <p>The map provides information in a context familiar to the author, as the viewer would most probably know the general layout of Chicago from looking at a map. This is the first scene that the user would be presented with.</p>
                         <p>A scatter plot populates the second scene, allowing the user to see the relationship between unemployment and homicides in various Chicago neighborhoods. The relationship itself is made more explicit by the trend line, calculated by a simple linear regression operation.</p>
                         <p>Each community is also represented in the bar chart that lies under the map and scatter plot, allowing the user to drill down into the details of the selected metric for each community, whether it is unemployment or homicides.</p>
 
+                        <br/>
+
                         <h3>Parameters: Radio Buttons</h3>
                         <p>The main parameter in the visualization is the radio button that allows the user to switch between two metrics: “Homicides per 100k” and “Unemployment %”. When the user switches their selection from one metric to another, the visualizations and scenes rerender to provide information about the selected metric for each neighborhood. </p>
+
+                        <br/>
 
                         <h3>Annotations and Triggers: Tooltips, mouse-over and click events</h3>
                         <p>When the user mouses over each neighborhood in the map, each dot in the plot, or each bar in the bar chart, a tooltip appears detailing the information about the selected metric for the respective neighborhood.</p>
@@ -71,6 +85,17 @@ class App extends Component {
                                 <VizParent height={1000} width={1000} />
                             </div>
                         </div>
+
+                        <h3>Conclusion</h3>
+                        <p>Looking at the data itself in the context of a scatter plot, where the x axis represents unemployment and the y axis represents homicides, there exists a relationship between unemployment and homicides. Note that the slope of the line of best fit corresponds to a positive correlation.</p>
+                        <p>The formula for the linear regression is as follows:
+                            <ul>
+                                <li>Assault (Homicide) = 1.91903 * Unemployment - 7.46172</li>
+                                <li>R-Squared: 0.663956</li>
+                                <li>P-value: Less than 0.0001</li>
+                            </ul>
+                        </p>
+                        <p>Although a middle R-squared value indicates the lack of explanation for a lot of the variation, such a low p value further validates the trend, as it indicates the correlation is statistically relevant. In other words, given the unemployment rate of a Chicago neighborhood, you can accurately predict its homicide rate.</p>
                         {/*<p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>*/}
                         {/*<div className="row">*/}
                             {/*<div className="6u 12u(mobilep)">*/}
@@ -84,20 +109,11 @@ class App extends Component {
                         {/*</div>*/}
                     </div>
                 </section>
-                {/*<!--&lt;!&ndash; Footer &ndash;&gt;-->*/}
-                {/*<!--<footer id="footer">-->*/}
-                {/*<!--<ul class="icons">-->*/}
-                {/*<!--<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>-->*/}
-                {/*<!--<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>-->*/}
-                {/*<!--<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>-->*/}
-                {/*<!--<li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>-->*/}
-                {/*<!--<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>-->*/}
-                {/*<!--<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>-->*/}
-                {/*<!--</ul>-->*/}
-                {/*<!--<ul class="copyright">-->*/}
-                {/*<!--<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>-->*/}
-                {/*<!--</ul>-->*/}
-                {/*<!--</footer>-->*/}
+                <footer id="footer">
+                    <ul className="copyright">
+                        <li>Design: <a href="http://html5up.net/alpha">HTML5 UP</a></li>
+                    </ul>
+                </footer>
 
             </div>
         );
